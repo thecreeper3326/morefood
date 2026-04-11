8966dc8d90e2fdf99f772c9c735d582dd96cc1d3
judah <judah.violinmaster@gmail.com>
2026-04-11 16:09:00 -0400

    Update Morefood initialization to use Log4j Logger and add more logging during initialization.

--- src/main/java/net/creeperdev/morefood/Morefood.java
+++ src/main/java/net/creeperdev/morefood/Morefood.java
@@ -1,16 +1,19 @@
 package net.creeperdev.morefood;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 public class Morefood implements ModInitializer {
-
+    public Logger LOGGER = LogManager.getLogger("MoreFood");
 
     @Override
     public void onInitialize() {
-
-        System.out.println("[Morefood] Initializing MoreFood by TheCreeper3326");
+        LOGGER.info("Initializing MoreFood");
         ModItems.initialize();
+        LOGGER.info("Done");
+
 
     }
 
